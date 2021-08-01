82d2d41b60f6564056b4a97bea9b6e745cfd9b6b
andrejtarasov <tarasov_av@cs.vsu.ru>
2021-08-01 22:17:02 +0300

    started flyway

---
+++ src/main/resources/db/migration/1_1_0/V1_1_0_3__create_table_participant.sql
@@ -0,0 +1,21 @@
+/* Single line comment */
+CREATE TABLE participant
+(
+    id uuid DEFAULT uuid_generate_v4() NOT NULL ,
+    name VARCHAR ,
+    surname VARCHAR ,
+    gender gender,
+    phone_number VARCHAR ,
+    status status,
+    intensive_experience INT,
+    voronezh_intensive_experience INT,
+    t_shirt_size INT
+);
+
+CREATE UNIQUE index participant_id_uindex
+	ON participant (id);
+
+ALTER TABLE participant
+    ADD CONSTRAINT participant_pk
+        PRIMARY KEY (id);
+
