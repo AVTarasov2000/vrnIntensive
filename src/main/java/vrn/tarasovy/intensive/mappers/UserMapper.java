b5f417b6d7eab71285cdf36233c814ee5a9415ed
avtarasov <tarasov_av@cs.vsu.ru>
2021-08-04 17:32:34 +0300

    mappers start...

--- src/main/java/vrn/tarasovy/intensive/mappers/UserMapper.java
+++ src/main/java/vrn/tarasovy/intensive/mappers/UserMapper.java
@@ -9,9 +9,6 @@ import vrn.tarasovy.intensive.models.User;
 @Mapper(config = IgnoreUnmappedMapperConfig.class,
         uses = {})
 public interface UserMapper extends AbstractMapper<UserDto, User> {
-    @Mapping(target = "legalEntity.id", source = "legalEntityId")
     User toDao(UserDto dto);
-
-    @Mapping(target = "legalEntityId", source = "legalEntity.id")
     UserDto toDto(User dao);
 }
---
+++ src/main/java/vrn/tarasovy/intensive/repository/UserRepository.java
@@ -0,0 +1,34 @@
+package vrn.tarasovy.intensive.repository;
+
+public interface UserRepository {
+}
+/*
+@Repository
+public interface WelderRepository extends PagingAndSortingRepository<Welder, UUID>, JpaSpecificationExecutor<Welder> {
+
+    Optional<Welder> findByBrand(String brand);
+
+    @Query(nativeQuery = true , value = "SELECT distinct ww.*, w.* " +
+            "FROM {h-schema}admission_welder_welding_type awwt \n" +
+            "INNER JOIN {h-schema}admission_welder aw ON awwt.admission_id = aw.id \n" +
+            "INNER JOIN {h-schema}worker_welder ww ON aw.welder_id = ww.id \n"  +
+            "INNER JOIN {h-schema}worker w ON w.id = ww.id AND w.legal_entity_id=:legalEntityId \n " +
+            "INNER JOIN {h-schema}dictionary_value dv ON w.status = dv.id AND dv.CODE='WORKING' \n " +
+            "INNER JOIN {h-schema}naks_welder nw ON nw.welder_id = ww.id and nw.welding_type=:weldingTypeId \n" +
+            "INNER JOIN {h-schema}naks n ON nw.id = n.id and n.certification_date_to > now() \n" +
+            "WHERE awwt.welding_type_id=:weldingTypeId")
+    List<Welder> findListWelderForJoints(@Param("legalEntityId") UUID legalEntityId, @Param("weldingTypeId") UUID weldingTypeId);
+
+    List<Welder> findAllByIdIn(List<UUID> ids);
+
+    @Query("select distinct layer.welder from ControlWeldedJointContent cwj join ControlWeldedJointLayer layer on layer.content = cwj")
+    List<Welder> getAllFromCwjContent();
+
+}
+
+
+@NoRepositoryBean
+public interface AbstractFileRepository<D extends FileContainer>  extends CrudRepository<D, FilePK> {
+    List<D> findByKeyEntityId(UUID entityId);
+}
+* */
