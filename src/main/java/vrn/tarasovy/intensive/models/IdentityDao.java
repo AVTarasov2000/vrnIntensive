82f466501ee95ed7a539799b68b23b26a02b3a14
andrejtarasov <tarasov_av@cs.vsu.ru>
2021-08-05 21:58:38 +0300

    user test dao

--- src/main/java/vrn/tarasovy/intensive/models/IdentityDao.java
+++ src/main/java/vrn/tarasovy/intensive/models/IdentityDao.java
@@ -1,6 +1,5 @@
 package vrn.tarasovy.intensive.models;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
