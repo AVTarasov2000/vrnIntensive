f8540a0a03c7f57dc9ff66bb0c94f54a52f30db3
andrejtarasov <tarasov_av@cs.vsu.ru>
2021-09-13 20:55:20 +0300

    @Repository

--- src/main/java/vrn/tarasovy/intensive/repository/PlaceRepository.java
+++ src/main/java/vrn/tarasovy/intensive/repository/PlaceRepository.java
@@ -1,11 +1,13 @@
 package vrn.tarasovy.intensive.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import vrn.tarasovy.intensive.models.place.Place;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface PlaceRepository extends CrudRepository <Place, UUID> {
     List <Place> findAll();
 }
